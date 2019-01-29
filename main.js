e0606ff7d45f00bb6c75203ed847370a409d328b
Viktor Svensson <42525041+viktorsve@users.noreply.github.com>
2019-01-29 13:20:33 +0100

    Cleaning up

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-import {test} from './js/calc_buttons.js';
-test();
+// import {
+//   test
+// } from "./js/calc_buttons.js";
+import {
+  numberFunction
+} from "./js/numberbuttons.js";
+// test();
+numberFunction();
