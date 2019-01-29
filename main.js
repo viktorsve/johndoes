a6f4685acbc883781a60a3b4ad186dc131647eab
maccydrums <marcusprivat@live.com>
2019-01-29 08:55:32 +0100

    marcus - user stories 1

---
+++ main.js
@@ -0,0 +1,19 @@
+var a = document.querySelector(".down");
+a.addEventListener('click', function() {
+	console.log("down");
+}, false);
+
+var b = document.querySelector(".up");
+b.addEventListener('click', function() {
+	console.log("up");
+}, false);
+
+var c = document.querySelector(".left");
+c.addEventListener('click', function() {
+	console.log("left");
+}, false);
+
+var d = document.querySelector(".right");
+d.addEventListener('click', function() {
+	console.log("right");
+}, false);
