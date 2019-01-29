1aff8d850b83d96e07cba68458bae17933e5b128
Oskar Olsson <oskarolsson86@gmail.com>
2019-01-29 11:29:24 +0100

    Test

---
+++ js/calc_buttons.js
@@ -0,0 +1,46 @@
+// Skapa +, - och *
+
+function test() {
+  $(".plus").click(function() {
+    $(".screen").append("+");
+  });
+  $(".minus").click(function(){
+    $(".screen").append("-");
+  });
+  $(".multi").click(function(){
+    $(".screen").append("*");
+  });
+}
+
+export {test};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
