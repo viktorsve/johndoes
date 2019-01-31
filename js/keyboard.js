5e837820f37c8cc348718c887f210a0fc546b586
maccydrums <marcusprivat@live.com>
2019-01-31 08:51:36 +0100

    fix

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,27 +1,32 @@
 export function myEvent(){
   document.addEventListener('keydown', keyHandler, false);
-  function keyHandler(e) {
-    if(e.keyCode == 49) {
+
+  function keyHandler(event) {
+    if(event.keyCode == 49) {
       $(".screen").append(1);
-    } else if (e.keyCode == 50) {
+    } else if (event.keyCode == 50) {
       $(".screen").append(2);
-    } else if (e.keyCode == 51) {
+    } else if (event.keyCode == 51) {
       $(".screen").append(3);
-    } else if (e.keyCode == 52) {
+    } else if (event.keyCode == 52) {
       $(".screen").append(4);
-    } else if (e.keyCode == 53) {
+    } else if (event.keyCode == 53) {
       $(".screen").append(5);
-    } else if (e.keyCode == 54) {
+    } else if (event.keyCode == 54) {
       $(".screen").append(6);
-    } else if (e.keyCode == 55) {
+    } else if (event.keyCode == 55) {
       $(".screen").append(7);
-    } else if (e.keyCode == 56) {
+    } else if (event.keyCode == 56) {
       $(".screen").append(8);
-    } else if (e.keyCode == 57) {
+    } else if (event.keyCode == 57) {
       $(".screen").append(9);
-    } else if (e.keyCode == 48) {
+    } else if (event.keyCode == 48) {
       $(".screen").append(0);
-    } else if (event.shiftKey && e.keyCode == 48) {
+    } else if (event.keyCode == 189) {
+      $(".screen").append("-");
+    } else if (event.keyCode == 187) {
+      $(".screen").append("+");
+    } else if (event.keyCode == 61) {
       $(".screen").append("=");
     }
   }
