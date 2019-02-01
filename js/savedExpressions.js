9864a689770c7965b37431ad9638eab54d9d6d8a
Jonas Forsberg <jonasf@Jonass-MacBook-Pro.local>
2019-02-01 10:53:05 +0100

    new js function

---
+++ js/savedExpressions.js
@@ -0,0 +1,10 @@
+let calcScreen = $('.screen')[0];
+let savedArray = [];
+
+$('.equal').click(function() {
+  if (calcScreen.innerHTML != "Error") {
+    if (savedArray.length <=) {
+
+    }
+  }
+});
