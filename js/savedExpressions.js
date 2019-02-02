03e8e921e454ea12a73fe6725d1d46b25a80972c
Jonas Forsberg <jonasf@Jonass-MacBook-Pro.local>
2019-02-02 01:25:49 +0100

    fixed list with saved expressions/changes in css and html

--- js/savedExpressions.js
+++ js/savedExpressions.js
@@ -1,10 +1,18 @@
-let calcScreen = $('.screen')[0];
 let savedArray = [];
 
-$('.equal').click(function() {
-  if (calcScreen.innerHTML != "Error") {
-    if (savedArray.length <=) {
+export function saveExpression() {
 
+  let calcScreen = $('.screen')[0];
+  if (calcScreen.innerHTML != "Error" || calcScreen.innerHTML != "Infinity") {
+    if (savedArray.length == 50) {
+      savedArray.pop();
+      savedArray.unshift(calcScreen.innerHTML);
+      $('ol').prepend("<li>" + 'Svar: ' + savedArray[0] + "</li>");
+      $('ol li:nth-child(50)').remove();
+    } else {
+      savedArray.unshift(calcScreen.innerHTML);
+      $('ol').prepend("<li>" + 'Svar: ' + savedArray[0] + "</li>");
     }
   }
-});
+  console.log(savedArray);
+}
