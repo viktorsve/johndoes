51f077729b9119b0915e207e3a6cdfa16928d115
Oskar Olsson <oskarolsson86@gmail.com>
2019-02-08 12:17:44 +0100

    added main-js

---
+++ main.js
@@ -0,0 +1,33 @@
+import {
+  equalFunction,
+  checkNumbers
+} from "./js/equalbutton.js";
+
+equalFunction();
+checkNumbers();
+
+import {
+  removeLastChar
+} from "./js/undo-button.js";
+
+removeLastChar();
+
+import {
+  timerFunction
+} from "./js/timerfunction.js";
+timerFunction();
+
+import {
+  myEvent
+} from "./js/keyboard.js";
+myEvent();
+
+import {
+
+  storeExpressions
+} from './js/store.js';
+storeExpressions();
+import {
+  chatFunction
+} from './js/chat.js';
+chatFunction();
